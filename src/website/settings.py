79bdff0358291f854e222c6ea1059fe7628de3b8
Tabigat Karbayev <tab1k@MacBook-Pro-Tabigat.local>
2025-03-26 22:53:04 +0500

    fourth commit

--- src/website/settings.py
+++ src/website/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'steelfed_db',
+        'USER': 'tab1k',
+        'PASSWORD': 'TOBI8585',
+        'HOST': 'postgres',  # Должно совпадать с именем контейнера
+        'PORT': '5432',
     }
 }
 
@@ -111,6 +122,8 @@ TIME_ZONE = "Asia/Almaty"
 USE_I18N = True
 
 USE_TZ = True
+
+DEBUG_PROPAGATE_EXCEPTIONS = False
 
 
 # Static files (CSS, JavaScript, Images)
